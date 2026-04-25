fa738da9f35cebf6860168717d925f5c5f26be59
paper209 <fleshed-stroll84@icloud.com>
2026-04-25 10:36:06 +0900

    Add parser.h header file with parsing function declarations

---
+++ parser.h
@@ -0,0 +1,13 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "filter.h"
+
+uint32_t parse_mask(char *mask);
+uint8_t parse_protocol(char *protocol_str);
+uint16_t parse_port(char *port_str);
+int parse_port_filter(char *token, struct port_filter *filter);
+int parse_netmask_filter(char *token, struct netmask_filter *filter);
+struct filter *read_and_parse(char *path, size_t *filters_len);
+
+#endif
