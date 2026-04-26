6f207ba0f203f1c2db28606349af07829041b961
paper209 <fleshed-stroll84@icloud.com>
2026-04-27 04:24:13 +0900

    Add new mask parsing functions to parser.h

--- parser.h
+++ parser.h
@@ -3,7 +3,8 @@
 
 #include "filter.h"
 
-uint32_t parse_mask(char *mask);
+uint32_t parse_mask_to_32(char *mask);
+char *parse_mask_to_str(uint32_t mask);
 uint8_t parse_protocol(char *protocol_str);
 uint16_t parse_port(char *port_str);
 int parse_port_filter(char *token, struct port_filter *filter);
