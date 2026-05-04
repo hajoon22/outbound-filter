97b2cb818ef5bbc1e0b553bc4f2a0b72164244a1
paper209 <fleshed-stroll84@icloud.com>
2026-05-04 11:17:23 +0900

    Clean up parser.h by removing unused functions
    Removed unused function declarations from parser.h.

--- parser.h
+++ parser.h
@@ -3,12 +3,6 @@
 
 #include "filter.h"
 
-uint32_t parse_mask_to_32(char *mask);
-char *parse_mask_to_str(uint32_t mask);
-uint8_t parse_protocol(char *protocol_str);
-uint16_t parse_port(char *port_str);
-int parse_port_filter(char *token, struct port_filter *filter);
-int parse_netmask_filter(char *token, struct netmask_filter *filter);
 struct filter *read_and_parse(char *path, size_t *filters_len);
 
 #endif
