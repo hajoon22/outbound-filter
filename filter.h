ee15dfbb4c533f105f685b8295856a4fdc4ccadc
paper209 <fleshed-stroll84@icloud.com>
2026-05-06 09:24:11 +0900

    Add signature filter to filter.h

--- filter.h
+++ filter.h
@@ -5,10 +5,12 @@
 
 #define PORT_FILTER_STR "port"
 #define NETMASK_FILTER_STR "netmask"
+#define SIGNATURE_FILTER_STR "signature"
 
 enum filter_type {
     PORT_FILTER = 0,
     NETMASK_FILTER = 2,
+    SIGNATURE_FILTER = 4,
 };
 
 enum {
@@ -29,11 +31,17 @@ struct netmask_filter {
     uint32_t mask;
 };
 
+struct signature_filter {
+    char *signature;
+    size_t signature_len;
+};
+
 struct filter {
     enum filter_type type;
     union {
         struct port_filter port;
         struct netmask_filter netmask;
+        struct signature_filter signature;
     };
 };
 
